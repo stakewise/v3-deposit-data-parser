03b081ba81ed7668bdce990f5c2eacfaaafcb8bf
dfkadyr <dfkadyr@gmail.com>
2024-03-01 12:32:26 +0300

    [worker]: replace await import to require

--- src/parser/initBls.ts
+++ src/parser/initBls.ts
@@ -1,5 +1,6 @@
 const initBls = async () => {
-  const bls = await import('bls-eth-wasm')
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const bls = require('bls-eth-wasm')
 
   await bls.init(bls.BLS12_381)
 
