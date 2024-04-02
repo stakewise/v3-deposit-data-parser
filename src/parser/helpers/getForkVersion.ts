a04874128d0687a5685a2e849498d0eafa39afc6
dfkadyr <dfkadyr@gmail.com>
2024-04-02 18:11:47 +0300

    [fix]: test import 10

--- src/parser/helpers/getForkVersion.ts
+++ src/parser/helpers/getForkVersion.ts
@@ -5,9 +5,10 @@ import type { SupportedNetworks } from '../types'
 // The values are in hexadecimal format and are intended to determine
 // the domain when signing messages in the context of Ethereum 2.0
 const forkVersions: Record<SupportedNetworks, string> = {
-  'holesky': '0x01017000',
-  'mainnet': '0x00000000',
-  'gnosis': '0x00000064',
+  'holesky': '0x4268',
+  'mainnet': '0x1',
+  'gnosis': '0x64',
+  'chiado': '0x27D8',
 }
 
 const getForkVersion = (network: SupportedNetworks) => getBytes(forkVersions[network])
