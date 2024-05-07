a1b20097ec71dd23864bf5723c0d88b7883a3f6f
dfkadyr <dfkadyr@gmail.com>
2024-05-07 13:31:19 +0300

    [amount]: update amount

--- src/parser/helpers/getAmount.ts
+++ src/parser/helpers/getAmount.ts
@@ -4,8 +4,8 @@ import type { SupportedNetworks } from '../types'
 const amounts: Record<SupportedNetworks, number> = {
   'holesky': 32000000000,
   'mainnet': 32000000000,
-  'gnosis': 1000000000,
-  'chiado': 1000000000,
+  'gnosis': 32000000000,
+  'chiado': 32000000000,
 }
 
 const getAmount = (network: SupportedNetworks) => amounts[network]
