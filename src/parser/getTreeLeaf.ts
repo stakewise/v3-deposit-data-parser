8f4325e8668fdb301dbe0be38e281ba72084ecc0
Kadyr Dzhemaledinov <dfkadyr@gmail.com>
2024-07-23 16:43:54 +0300

    New tree (#11)
    * add withdrawalAddress to merkle tree
    
    * 1.8.7
    
    * show restake logic
    
    * 1.8.8
    
    * add withdrawalAddress props
    
    * 1.8.9

--- src/parser/getTreeLeaf.ts
+++ src/parser/getTreeLeaf.ts
@@ -6,12 +6,13 @@ export type TreeLeafInput = {
   pubkey: string
   signature: string
   depositData: DepositData
+  withdrawalAddress?: string
 }
 
-// Creates an array concatenating pubkey, signature, and hashTreeRoot for a given deposit data
+// Creates an array concatenating pubkey, signature, hashTreeRoot and withdrawalAddress for a given deposit data
 // This array is used to create a Merkle tree and load it into IPFS
 const getTreeLeaf = (values: TreeLeafInput): Uint8Array => {
-  const { depositData, pubkey, signature } = values
+  const { depositData, pubkey, signature, withdrawalAddress } = values
 
   try {
     // Calculate the hash tree root for the given deposit data and signature
@@ -20,12 +21,18 @@ const getTreeLeaf = (values: TreeLeafInput): Uint8Array => {
       signature: getBytes(prefix0x.add(signature)),
     })
 
-    const leave = Buffer.concat([
+    const leafParts = [
       getBytes(prefix0x.add(pubkey)),
       getBytes(prefix0x.add(signature)),
-    ])
+      hashTreeRoot,
+    ]
 
-    const treeLeaf = Buffer.concat([ leave, hashTreeRoot ])
+    // Optionally add the withdrawal address if it exists
+    if (withdrawalAddress) {
+      leafParts.push(getBytes(prefix0x.add(withdrawalAddress)))
+    }
+
+    const treeLeaf = Buffer.concat(leafParts)
 
     return treeLeaf
   }
