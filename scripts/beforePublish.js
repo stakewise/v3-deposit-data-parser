ad3503d27173cccd4a5edab1800f13f958fe3837
Kadyr Dzhemaledinov <dfkadyr@gmail.com>
2024-08-29 17:16:35 +0300

    remove husky, update lock file (#12)
    * remove husky, update lock file
    
    * remove log

---
+++ scripts/beforePublish.js
@@ -0,0 +1,21 @@
+const { execSync } = require('child_process')
+const package = require('../package.json')
+
+
+const check = async () => {
+  const npmVersion = package.version
+  const releaseVersion = execSync(`git ls-remote https://github.com/stakewise/v3-deposit-data-parser refs/tags/v${npmVersion}`, { encoding: 'utf8' })
+    .replace(/.*\//, '')
+    .replace(/v/g, '')
+    .trim()
+
+  if (npmVersion !== releaseVersion || !releaseVersion) {
+    console.error(`Please add ${npmVersion} release to Github https://github.com/stakewise/v3-deposit-data-parser/releases/new`)
+    process.exit(1)
+  }
+  else {
+    process.exit(0)
+  }
+}
+
+check()
