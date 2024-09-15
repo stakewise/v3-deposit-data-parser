a6d36702cfec45e8973a605cd0def0029dedb666
Kadyr Dzhemaledinov <dfkadyr@gmail.com>
2024-09-15 09:25:31 +0300

    update gnosis url (#14)

--- src/parser/helpers/requests/urls.ts
+++ src/parser/helpers/requests/urls.ts
@@ -4,6 +4,6 @@ import type { SupportedNetworks } from '../../types'
 export const urls: Record<SupportedNetworks, string> = {
   'holesky': 'https://holesky-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
   'mainnet': 'https://mainnet-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
-  'gnosis': 'https://graph-gno.stakewise.io/subgraphs/name/stakewise/stakewise',
+  'gnosis': 'https://gnosis-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
   'chiado': 'https://chiado-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
 }
