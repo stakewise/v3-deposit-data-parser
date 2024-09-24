50b99455251ce4140f91d1ef72530d59132f0326
CAst <programmer.andrey@gmail.com>
2024-09-24 17:21:19 +0500

    [subgraph-tvl-apy] change urls (#19)

--- src/parser/helpers/requests/urls.ts
+++ src/parser/helpers/requests/urls.ts
@@ -2,9 +2,8 @@ import type { SupportedNetworks } from '../../types'
 
 
 export const urls: Record<SupportedNetworks, string> = {
-  'holesky': 'https://graphs.stakewise.io/holesky/subgraphs/name/stakewise/prod',
-  // TODO replace with https://graphs.stakewise.io/mainnet/subgraphs/name/stakewise/prod
-  'mainnet': 'https://graphs.stakewise.io/mainnet-stage/subgraphs/name/stakewise/prod',
-  'gnosis': 'https://gnosis-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
-  'chiado': 'https://chiado-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
+  'holesky': 'https://graphs.stakewise.io/mainnet/subgraphs/name/stakewise/prod',
+  'mainnet': 'https://graphs.stakewise.io/holesky/subgraphs/name/stakewise/prod',
+  'gnosis': 'https://graphs.stakewise.io/gnosis/subgraphs/name/stakewise/prod',
+  'chiado': 'https://graphs.stakewise.io/chiado/subgraphs/name/stakewise/prod',
 }
