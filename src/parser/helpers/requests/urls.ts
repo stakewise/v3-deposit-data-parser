c7c656f06b5305053044b131d699d7423946e39b
Mike Diamond <info@mdiamond.ru>
2024-09-24 13:26:23 +0300

    [new subgraph] update schema (#18)
    * [new subgraph] update schema
    
    * [new subgraph] update urls

--- src/parser/helpers/requests/urls.ts
+++ src/parser/helpers/requests/urls.ts
@@ -2,8 +2,9 @@ import type { SupportedNetworks } from '../../types'
 
 
 export const urls: Record<SupportedNetworks, string> = {
-  'holesky': 'https://holesky-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
-  'mainnet': 'https://mainnet-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
+  'holesky': 'https://graphs.stakewise.io/holesky/subgraphs/name/stakewise/prod',
+  // TODO replace with https://graphs.stakewise.io/mainnet/subgraphs/name/stakewise/prod
+  'mainnet': 'https://graphs.stakewise.io/mainnet-stage/subgraphs/name/stakewise/prod',
   'gnosis': 'https://gnosis-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
   'chiado': 'https://chiado-graph.stakewise.io/subgraphs/name/stakewise/stakewise',
 }
