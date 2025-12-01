33bbb65b0ed39a1acde9b9fa16b37346d2b4d7dd
dfkadyr-stakewise <kadyr@stakewise.io>
2025-12-01 15:23:54 +0300

    remove chiado network

--- src/parser/helpers/getAmount.ts
+++ src/parser/helpers/getAmount.ts
@@ -4,7 +4,6 @@ import type { SupportedNetworks } from '../types'
 const amounts: Record<SupportedNetworks, number> = {
   mainnet: 32000000000,
   gnosis: 32000000000,
-  chiado: 32000000000,
   hoodi: 32000000000,
 }
 
